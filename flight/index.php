bf262e8466880633ee35ccfea01686a042e2dca1
anajaci <ana.jacimovic@yahoo.com>
2020-01-30 13:33:14 +0100

    Update index.php

--- flight/index.php
+++ flight/index.php
@@ -1,7 +1,9 @@
 <?php
 require 'flight/Flight.php';
 require 'jsonindent.php';
-Flight::register('db', 'Database', array('citaonica'));
+Flight::register('db', 'PDO', array("mysql:host=localhost;port=3306;dbname=citaonica;",'root',''),function($db)
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+);
 $json_podaci = file_get_contents("php://input");
 Flight::set('json_podaci', $json_podaci );
 
@@ -9,58 +11,28 @@ Flight::route('/', function(){
     echo 'hello world!';
 });
 
-Flight::route('GET /student.json', function(){
-	header ("Content-Type: application/json; charset=utf-8");
-	$db = Flight::db();
-	$db->selectM();
-	$niz=array();
-	while ($red=$db->getResult()->fetch_object()){
-		$niz[] = $red;
+
+Flight::route('POST /register', function(){
+	$obj = json_decode(file_get_contents("php://input"));
+	if(isset($obj)) {
+		if(!empty($obj->idstudenta)&&!empty($obj->ime)&&!empty($obj->prezime)&&!empty($obj->brojindeksa)&&!empty($obj->vreme)) {
+			$db=Flight::db();
+
+			$result = $db->prepare("INSERT INTO studenti(idstudenta,ime,prezime,brojindeksa,vreme) VALUES 
+				(	'" .$obj->idstudenta . "',
+					'" .$obj->ime . "',
+					'" .$obj->prezime . "',
+					'" .$obj->brojindeksa . "',
+					'" .$obj->vreme . "')");
+			$result->execute();
+
+			// $result1 = $db->query("SELECT * FROM studenti");
+			// if($result1->rowCount() >= 1) {
+			// 	header ("Content-Type: application/json; charset=utf-8");
+			// 	echo "";
+			// }
+		}
 	}
-
-	$json_niz = json_encode ($niz,JSON_UNESCAPED_UNICODE);
-	echo indent($json_niz);
-	return false;
-});
-
-
-	
-Flight::route('POST /citaonica.json', function(){
-	header ("Content-Type: application/json; charset=utf-8");
-	$db = Flight::db();
-	$podaci_json = Flight::get("json_podaci");
-	$podaci = json_decode ($podaci_json);
-	
-	if ($podaci == null){
-		$odgovor["poruka"] = "Niste prosledili podatke";
-		$json_odgovor = json_encode ($odgovor);
-		echo $json_odgovor;
-		}
-	
-	else {
-
-		$podaci_query = array();
-		foreach ($podaci as $k=>$v){
-			$v = "'".$v."'";
-			$podaci_query[$k] = $v;
-		}
-	if ($db->insert("studenti", "idstudenta, ime, prezime, brojindeksa", array($podaci_query["idstudenta"],$podaci_query["ime"],$podaci_query["prezime"],$podaci_query["brojindeksa"]))){				
-				$odgovor["poruka"] = "Student je ucitan";
-				$json_odgovor = json_encode ($odgovor,JSON_UNESCAPED_UNICODE);
-				echo $json_odgovor;
-				return false;
-			} 
-		
-		else {
-				$odgovor["poruka"] = "Došlo je do greške pri ubacivanju merenja";
-				$json_odgovor = json_encode ($odgovor,JSON_UNESCAPED_UNICODE);
-				echo $json_odgovor;
-				return false;
-			}
-		
-	}	
-
-
-	});
+}
 
 Flight::start();
